268fd49cb7d7999449d132e98c86fbcff55f7b74
Bipul Shahi <bipulshahi25@gmail.com>
2024-06-01 16:07:26 +0530

    Add files via upload

--- packaging_ml_model/Myloanapp/config/config.py
+++ packaging_ml_model/Myloanapp/config/config.py
@@ -6,9 +6,9 @@ import Myloanapp
 #set PYTHONPATH=C:\Users\bipulkumar\Documents\MLmodelPackaging\packaging_ml_model
 
 PACKAGE_ROOT = pathlib.Path(Myloanapp.__file__).resolve().parent
+print(PACKAGE_ROOT)
 
 DATAPATH = os.path.join(PACKAGE_ROOT,"datasets")
-
 print(DATAPATH)
 
 TRAIN_FILE = 'train_loan.csv'
@@ -16,6 +16,7 @@ TEST_FILE = 'test_loan.csv'
 
 MODEL_NAME = "classification.pkl"
 SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_models")
+print(SAVE_MODEL_PATH)
 
 TARGET = 'Loan_Status'
 
---
+++ packaging_ml_model/Myloanapp/training_pipeline.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+from Myloanapp.config import config
+from Myloanapp.processing.data_handling import load_dataset,save_pipeline
+import Myloanapp.processing.preprocessing as pp
+import Myloanapp.pipeline as pipe
